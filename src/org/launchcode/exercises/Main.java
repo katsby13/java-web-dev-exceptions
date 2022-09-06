7683011c2fb5f67da166c9dec279460e9837874f
Katlyn McDonald <kkmcoding@gmail.com>
2022-09-06 03:45:02 -0400

    Exercises complete

--- src/org/launchcode/exercises/Main.java
+++ src/org/launchcode/exercises/Main.java
@@ -1,31 +1,57 @@
 package org.launchcode.exercises;
 
 import java.util.HashMap;
-import java.util.Map;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        // Test out your Divide() function!
+        Divide(8,2);
+        Divide(8,0);
 
         HashMap<String, String> studentFiles = new HashMap<>();
         studentFiles.put("Carl", "Program.java");
         studentFiles.put("Brad", "");
         studentFiles.put("Elizabeth", "MyCode.java");
         studentFiles.put("Stefanie", "CoolProgram.java");
+        studentFiles.put("Katlyn", null);
 
-        // Test out your CheckFileExtension() function!
+        System.out.println(CheckFileExtension(studentFiles.get("Carl")));
+        System.out.println(CheckFileExtension(studentFiles.get("Brad")));
+        System.out.println(CheckFileExtension(studentFiles.get("Katlyn")));
     }
 
-    public static void Divide(int x, int y)
-    {
-        // Write code here!
+    public static void Divide(int x, int y) {
+        if (y == 0) {
+            try {
+                throw new ArithmeticException("Y cannot equal zero!");
+            } catch (ArithmeticException e) {
+                e.printStackTrace();
+            }
+        } else {
+            int sum = x/y;
+            System.out.println(sum);
+        }
     }
 
     public static int CheckFileExtension(String fileName)
     {
-        // Write code here!
+        int result = 0;
+        if (fileName == null) {
+            try {
+                throw new NullPointerException("File can't equal null!");
+            } catch (NullPointerException e) {
+                e.printStackTrace();
+                result = -1;}
+        } else if (fileName == "") {
+            try {
+                throw new Exception("Empty string present");
+            } catch (Exception e) {
+                e.printStackTrace();
+                result = -1;}
+        } else if (fileName.endsWith(".java")) {
+                result = 1;
+            }
+        return result;
     }
-
 }
